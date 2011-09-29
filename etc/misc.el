e70d39e0aa619fcf2bba62f715924622eedb1b5d
Tuomas Kareinen <tkareine@gmail.com>
2011-09-29 13:40:34 +0300

    Always prefer UTF-8

--- etc/misc.el
+++ etc/misc.el
@@ -1,3 +1,10 @@
+;; Prefer UTF-8 encoding for input and output
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
 ;; IDO: Flexible matching (try exact match first, then fuzzy)
 (setq ido-enable-flex-matching t)
 
