8763ba9cec5161a71ed95b155ea9db2a2335ce79
Tuomas Kareinen <tkareine@gmail.com>
2012-01-23 23:26:54 +0200

    Load Erlang lang support only if dirs exist

--- etc/programming.el
+++ etc/programming.el
@@ -6,10 +6,13 @@
        (bin-dir   (concat root-dir "/bin"))
        (elisp-dir (car (file-expand-wildcards (concat root-dir
                                                       "/lib/tools-2.6.*/emacs") t))))
-  (setq erlang-root-dir root-dir)
-  (add-to-list 'load-path elisp-dir)
-  (add-to-list 'exec-path bin-dir))
-(require 'erlang-start)
+  (when (and (file-exists-p bin-dir)
+             (file-exists-p elisp-dir))
+    (message "erlang start")
+    (setq erlang-root-dir root-dir)
+    (add-to-list 'load-path elisp-dir)
+    (add-to-list 'exec-path bin-dir)
+    (require 'erlang-start)))
 
 ;; JavaScript language support
 (my-add-dotfile-lib-to-load-path "js2-mode")
