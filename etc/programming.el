ecc34d057c073487b9e9e9df6a57c9d2c31bc6b2
Tuomas Kareinen <tkareine@gmail.com>
2012-01-23 23:26:41 +0200

    Fix regexp

--- etc/programming.el
+++ etc/programming.el
@@ -32,7 +32,7 @@
 ;; Sass language support
 (my-add-dotfile-lib-to-load-path "scss-mode")
 (autoload 'scss-mode "scss-mode")
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.scss$" . scss-mode))
 
 (setq scss-compile-at-save nil)
 
