e7a33a07a39b2696735f6ff3908f9e07a7daef96
Tuomas Kareinen <tkareine@gmail.com>
2012-01-23 17:44:15 +0200

    Disable scss file compilation at save, rely on project's config

--- etc/programming.el
+++ etc/programming.el
@@ -34,6 +34,8 @@
 (autoload 'scss-mode "scss-mode")
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 
+(setq scss-compile-at-save nil)
+
 ;; Scala language support
 (my-add-dotfile-lib-path "scala-mode")
 (require 'scala-mode-auto)
