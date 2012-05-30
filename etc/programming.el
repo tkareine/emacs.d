7729fb7fc1f1cc5fcac00186fb8893b4897e8250
Tuomas Kareinen <tkareine@gmail.com>
2012-05-30 18:43:54 +0300

    Remove js2-highlight-vars-mode, "Back to top level" is simply too frustrating

--- etc/programming.el
+++ etc/programming.el
@@ -31,11 +31,6 @@
  (lambda ()
    (local-set-key (kbd "C-c s") 'my-toggle-js2-strict-missing-semi-warning)))
 
-(add-hook 'js2-mode-hook
- (lambda ()
-   (require 'js2-highlight-vars)
-   (js2-highlight-vars-mode)))
-
 ;; Sass language support
 (my-add-dotfile-lib-to-load-path "scss-mode")
 (autoload 'scss-mode "scss-mode")
