6fa0681193e987a48ee78a8636c02cf21a605170
Tuomas Kareinen <tkareine@gmail.com>
2012-10-26 09:08:25 +0300

    Moar packages to my minimum set of packages

--- dependencies.el
+++ dependencies.el
@@ -1,5 +1,9 @@
 (defvar my-package-dependencies '(
+  clojure-mode
   color-theme
+  expand-region
   markdown-mode
+  starter-kit
   starter-kit-bindings
+  starter-kit-ruby
 ) "Minimum set of packages required for my configuration")
