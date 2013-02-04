5872420e61996abd926724af9a26c1cd925c5458
Tuomas Kareinen <tkareine@gmail.com>
2013-02-04 09:15:38 +0200

    Sort key bindings

--- etc/keys.el
+++ etc/keys.el
@@ -6,20 +6,20 @@
 (setq ns-function-modifier 'super)
 
 ;; Custom key bindings
-(global-set-key [kp-delete]    'delete-char)
-(global-set-key [M-kp-delete]  'kill-word)
+(global-set-key (kbd "C-M-b")  'my-backward-char-5)
+(global-set-key (kbd "C-M-f")  'my-forward-char-5)
+(global-set-key (kbd "C-M-n")  'my-next-line-5)
+(global-set-key (kbd "C-M-p")  'my-previous-line-5)
 (global-set-key (kbd "C-c c")  'comment-or-uncomment-region)
 (global-set-key (kbd "C-c p")  'my-file-path-to-clipboard)
 (global-set-key (kbd "C-c t")  'delete-trailing-whitespace)
 (global-set-key (kbd "C-c w")  'whitespace-mode)
-(global-set-key (kbd "s-SPC")  'hippie-expand)
 (global-set-key (kbd "M-j")    'my-join-line)
 (global-set-key (kbd "M-å")    'er/expand-region)
 (global-set-key (kbd "M-ö")    'fixup-whitespace)
-(global-set-key (kbd "C-M-n")  'my-next-line-5)
-(global-set-key (kbd "C-M-p")  'my-previous-line-5)
-(global-set-key (kbd "C-M-f")  'my-forward-char-5)
-(global-set-key (kbd "C-M-b")  'my-backward-char-5)
+(global-set-key (kbd "s-SPC")  'hippie-expand)
+(global-set-key [M-kp-delete]  'kill-word)
+(global-set-key [kp-delete]    'delete-char)
 
 ;; Auto complete: advanced completion
 ;; <http://cx4a.org/software/auto-complete/manual.html#auto-complete_command>
