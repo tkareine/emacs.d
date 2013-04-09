7547dbf7615709b5e8926f3d692aea9d02fae921
Tuomas Kareinen <tkareine@gmail.com>
2013-04-09 10:01:59 +0300

    Disable strict semicolons warning in js2-mode by default

--- etc/programming.el
+++ etc/programming.el
@@ -27,6 +27,7 @@
 (setq js2-concat-multiline-strings nil)
 (setq js2-highlight-level 3)
 (setq js2-missing-semi-one-line-override t)
+(setq js2-strict-missing-semi-warning nil)
 (custom-set-faces '(js2-private-member ((t (:foreground "coral1")))))
 
 (add-hook 'js2-mode-hook
