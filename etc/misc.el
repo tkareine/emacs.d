36b48283c3d06740fcf4a579f7e193a3ff21ae50
Tuomas Kareinen <tkareine@gmail.com>
2013-07-05 13:27:56 +0300

    Use set-language-environment to set UTF-8 the preferred encoding

--- etc/misc.el
+++ etc/misc.el
@@ -1,9 +1,5 @@
 ;; Prefer UTF-8 encoding for input and output
-(setq locale-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-selection-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
+(set-language-environment "UTF-8")
 
 ;; Dired: allow opening file, replacing current buffer
 (put 'dired-find-alternate-file 'disabled nil)
