e2ab1275fac194e71fa2d2a1765a062fafa2f10f
Tuomas Kareinen <tkareine@gmail.com>
2014-10-09 12:09:46 +0300

    Fix error handling in finding erlang-mode

--- etc/major-modes.el
+++ etc/major-modes.el
@@ -15,8 +15,9 @@
 (let* ((root-dir  (tkareine/dotfile-path "lib/erlang-mode"))
        (bin-dir   (concat root-dir "/bin"))
        (elisp-dir (car (file-expand-wildcards (concat root-dir
-                                                      "/lib/tools-2.6.*/emacs") t))))
+                                                      "/lib/tools-*/emacs") t))))
   (when (and (file-exists-p bin-dir)
+             elisp-dir
              (file-exists-p elisp-dir))
     (setq erlang-root-dir root-dir)
     (add-to-list 'load-path elisp-dir)
