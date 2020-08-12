027888333016cafaf3011c9774747faf5f50a5d8
Tuomas Kareinen <tkareine@gmail.com>
2020-08-12 23:06:13 +0300

    Require Emacs >= 26

--- init.el
+++ init.el
@@ -1,5 +1,8 @@
 ;; -*- lexical-binding: t; -*-
 
+(when (< emacs-major-version 26)
+  (error "Emacs version >= 26 required (current is %s)" emacs-version))
+
 (load (expand-file-name "site-lisp/tk-init" user-emacs-directory))
 
 (setq custom-file (tk-init/user-emacs-path "custom.el"))
