997bb25ca832e8ee5d4bee3790bc2ba04ad572cb
Tuomas Kareinen <tkareine@gmail.com>
2020-08-12 23:10:29 +0300

    Set read-process-output-max to 1 MiB

--- etc/tk-internals.el
+++ etc/tk-internals.el
@@ -5,3 +5,6 @@
 ;; lsp-mode. From
 ;; `https://emacs-lsp.github.io/lsp-mode/page/performance/'.
 (customize-set-variable 'gc-cons-threshold 10000000)
+
+;; Maximum number of bytes to read from subprocess in a single chunk.
+(setq read-process-output-max (* 1024 1024))
