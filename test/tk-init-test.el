e3e4d6fbd92369f24c8c77298c3972b2cbb6d544
Tuomas Kareinen <tkareine@gmail.com>
2021-02-17 10:22:06 +0200

    Fix indentation

--- test/tk-init-test.el
+++ test/tk-init-test.el
@@ -5,7 +5,7 @@
 (ert-deftest tk-init/user-emacs-path-test ()
   (should (equal (tk-init/user-emacs-path) (expand-file-name "" user-emacs-directory)))
   (should (equal (tk-init/user-emacs-path "") (expand-file-name "" user-emacs-directory)))
-(should (equal (tk-init/user-emacs-path "a") (expand-file-name "a" user-emacs-directory)))
+  (should (equal (tk-init/user-emacs-path "a") (expand-file-name "a" user-emacs-directory)))
   (should (equal (tk-init/user-emacs-path "a/b") (expand-file-name "a/b" user-emacs-directory)))
   (should (equal (tk-init/user-emacs-path "a" "b") (expand-file-name "a/b" user-emacs-directory)))
   (should (equal (tk-init/user-emacs-path "a" "b/c" "d/e") (expand-file-name "a/b/c/d/e" user-emacs-directory))))
