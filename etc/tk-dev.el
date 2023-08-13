b9bed6264b5e50621f0f64b54d6fd5f3c4f0de71
Tuomas Kareinen <tkareine@gmail.com>
2023-08-13 16:15:51 +0300

    Remove rest-client

--- etc/tk-dev.el
+++ etc/tk-dev.el
@@ -565,10 +565,3 @@ configuration for GNU Global."
 (add-to-list 'auto-mode-alist '("/\\.aws/credentials\\'" . conf-unix-mode))
 (add-to-list 'auto-mode-alist '("/\\.s3cfg\\'" . conf-unix-mode))
 (add-to-list 'auto-mode-alist '("/Cargo\\.lock\\'" . conf-toml-mode))
-
-;;; restclient
-
-(use-package restclient
-  :ensure t
-
-  :defer t)
