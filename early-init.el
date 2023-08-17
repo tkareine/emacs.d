ec1e052dc3e5601f247ad85bbb1d85f842cd4c78
Tuomas Kareinen <tkareine@gmail.com>
2023-08-17 15:11:34 +0300

    Use more readable code for big values

--- early-init.el
+++ early-init.el
@@ -4,7 +4,9 @@
 ;; from the original value to avoid frequent GC pauses with
 ;; lsp-mode. From
 ;; `https://emacs-lsp.github.io/lsp-mode/page/performance/'.
-(setq-default gc-cons-threshold (* 10 1000 1000))
+(setq-default gc-cons-threshold
+              (let ((mb (* 1024 1024))) (* 10 mb)))
 
 ;; Maximum number of bytes to read from subprocess in a single chunk.
-(setq-default read-process-output-max (* 1024 1024))
+(setq-default read-process-output-max
+              (let ((mb (* 1024 1024))) (* 1 mb)))
