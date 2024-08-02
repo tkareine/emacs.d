093c2feec273d2fcdac601fdd3eb7392708017c8
Tuomas Kareinen <tkareine@gmail.com>
2024-08-02 11:53:19 +0300

    Simplify mode specification

--- etc/tk-dev.el
+++ etc/tk-dev.el
@@ -592,7 +592,7 @@ configuration for GNU Global."
 
 (use-package dockerfile-ts-mode
   :mode
-  ("/Dockerfile\\(?:\\.[^/]+\\)?\\'" . dockerfile-ts-mode))
+  ("/Dockerfile\\(?:\\.[^/]+\\)?\\'"))
 
 ;;; Configuration files
 
