d36f0827276c76808281f293ff0362037ee2cb13
Tuomas Kareinen <tkareine@gmail.com>
2026-02-18 19:58:43 +0200

    Add workaround for a possible bug of using find-file-noselect-1

--- init.el
+++ init.el
@@ -5,7 +5,13 @@
 
 (load (expand-file-name "site-lisp/tk-init" user-emacs-directory))
 
-(setq-default custom-file null-device)
+;; Avoid the following error when calling certain commands, such as
+;; `package-install':
+;;
+;;   find-file-noselect-1: Maximum buffer size exceeded
+;;
+;; See `https://lists.nongnu.org/archive/html/bug-gnu-emacs/2025-08/msg01404.html'
+(setq-default custom-file (expand-file-name "custom.el" user-emacs-directory))
 
 (dolist (l '("tk-network"
              "tk-packages"
